c5a168b8cf86a74a8600710f1b2e87a984eeeb83
Zygis911 <zygis.cer@gmail.com>
2024-09-12 22:52:01 +0300

    yes

---
+++ server/controllers/appointmentController.mjs
@@ -0,0 +1,48 @@
+import { validationResult } from "express-validator";
+import appointmentModel from "../models/appointmentModel.mjs";
+
+const appointmentController = {
+  createAppointment: async (req, res, next) => {
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+      console.log("user in request", req.user); // log user info for debugging
+
+      if (!req.user) {
+        return res.status(401).json("unauthorized access");
+      }
+      const { name, description } = req.body;
+      const createdAppointment = await appointmentModel.createAppointments(
+        name,
+        description
+      );
+      res.status(201).json(createdAppointment);
+    } catch (error) {}
+  },
+
+  getAppointmentById: async (req, res, next) => {
+    try {
+      if (!req.user) {
+        return res.status(401).json({ message: "unauthorized access" });
+      }
+
+      const appointmentId = req.params.id;
+
+      const appointment = await appointmentModel.getAppointmentById(
+        appointmentId
+      ); // appointment existence check
+
+      if (!appointment) {
+        return res.status(400).json({ message: "appointment does not exist" });
+      }
+
+      return res.status(200).json(appointment);
+    } catch (error) {
+        next(error);
+    }
+  },
+};
+
+export default appointmentController;
--- server/routes/mainRouter.mjs
+++ server/routes/mainRouter.mjs
@@ -1,6 +1,7 @@
 import express from 'express';
 // import dotenv from 'dotenv';
 import userRouter from './userRouter.mjs';
+import appointmentRouter from './appointmentRouter.mjs'
 
 // dotenv.config();
 const router = express.Router();
@@ -8,5 +9,5 @@ const router = express.Router();
 
 
 router.use('/users', userRouter)
-
+router.use('/appointments', appointmentRouter)
 export default router;
