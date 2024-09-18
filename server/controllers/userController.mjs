de6139e1edd7b01dcdfea3bae48b36e49f047599
Zygis911 <zygis.cer@gmail.com>
2024-09-18 22:24:57 +0300

    back to square 1, can do appointments when logged in

--- server/controllers/userController.mjs
+++ server/controllers/userController.mjs
@@ -1,16 +1,14 @@
-import argon2 from "argon2";
-import {V4 as paseto} from 'paseto';
+// import argon2 from "argon2";
+// import {V4 as paseto} from 'paseto';
+// import { promises as fs } from 'fs';
+// import { createPrivateKey } from 'crypto';
+import bcrypt from "bcrypt";
 import { validationResult } from "express-validator";
-import { promises as fs } from 'fs';
-import { createPrivateKey } from 'crypto';
-
-
+import jwt from 'jsonwebtoken'
 
 // import validation
 
 import userModel from "../models/userModel.mjs";
-
-
 
 const userController = {
   createUser: async (req, res, next) => {
@@ -22,7 +20,7 @@ const userController = {
 
       const { username, password, email } = req.body;
 
-      const hashedPassword = await argon2.hash(password);
+      const hashedPassword = await bcrypt.hash(password, 10);
 
       const newUser = {
         username,
@@ -64,7 +62,7 @@ const userController = {
       }
 
       // Check if passwords match
-      const passwordsMatch = await argon2.verify(user.password, password);
+      const passwordsMatch = await bcrypt.compare(password, user.password);
       if (!passwordsMatch) {
         return res.status(400).json({
           errors: [{ path: "password", msg: "incorrect password" }],
@@ -74,29 +72,13 @@ const userController = {
       // Remove sensitive information
       delete user.password;
 
-      // Load the private key from the file
-      const privateKeyPem = await fs.readFile(process.env.JWT_PRIVATE_KEY_PATH, 'utf8');
-      const privateKey = createPrivateKey({
-        key: privateKeyPem,
-        format: 'pem',
-        type: 'pkcs8',
+      // generating a token
+      const token = jwt.sign({ user }, process.env.JWT_SECRET, {
+        expiresIn: "2h",
       });
 
-      // Generate a unique PASETO token, including a timestamp and expiry time
-      const tokenPayload = {
-        user, // This will include the user data in the token
-        iat: Math.floor(Date.now() / 1000), // Issued at timestamp
-        exp: Math.floor(Date.now() / 1000) + (60 * 60 * 2), // Expiry timestamp (2 hours)
-      };
+      res.status(200).json({ message: "logged in successfully", token });
 
-      // Sign the token with the private key
-      const token = await paseto.sign(tokenPayload, privateKey);
-
-      // Send the token back to the client
-      res.status(200).json({
-        message: "user logged in successfully",
-        token,
-      });
     } catch (error) {
       next(error);
     }
@@ -104,23 +86,22 @@ const userController = {
 
   getUserById: async (req, res, next) => {
     try {
-        const id = req.params.id;
-        const user = await userModel.getUserById(id);
+      const id = req.params.id;
+      const user = await userModel.getUserById(id);
 
-        if(!user) {
-            res.status(400).json({message: "user not found"});
-        }
+      if (!user) {
+        res.status(400).json({ message: "user not found" });
+      }
 
-        delete user.password;
+      delete user.password;
 
-        res.status(200).json(user);
+      res.status(200).json(user);
     } catch (error) {
-        next(error);
+      next(error);
     }
   },
-  
 
-//search username bus
+  //search username bus
 };
 
 export default userController;
--- server/middleware/authMiddleware.mjs
+++ server/middleware/authMiddleware.mjs
@@ -1,37 +1,37 @@
-import {V4 as paseto} from 'paseto'
+// import {V4 as paseto} from 'paseto'
+import jwt from 'jsonwebtoken'
 import 'dotenv/config';
 import userModel from '../models/userModel.mjs';
 
 export default async function AuthMiddleware(req, res, next) {
     try {
-        //check if the token is provided in authorizaton
-        const authHeader = req.get('Authorization');
-        if(!authHeader) {
-            return res.status(401).json("unauthorized access");
-        }
-        
-        const token = authHeader.split('')[1];
-        if (!token) {
-            return res.status(401).json('Unauthorized access');
-        }
-
-         // Checking if the token is valid
-        const payload = await paseto.verify(token, process.env.JWT_PRIVATE_KEY_PATH);
-        if (!payload) {
-            return res.status(401).json('Unauthorized access');
-        }
-
-        // check if the user is valid 
-
-        const user = await userModel.getUserById(payload.user.id);
-        if (!user) {
-            throw new Error("user no longer exists");
-        }
-
-        // Providing user for the next in the pipeline
-        req.user = user;
-        next();
+      // Checking if token is provided in the Authorization header
+      const authHeader = req.get("Authorization");
+      if (!authHeader) {
+        return res.status(401).json("Unauthorized access");
+      }
+  
+      const token = authHeader.split(" ")[1];
+      if (!token) {
+        return res.status(401).json("Unauthorized access");
+      }
+  
+      // Checking if the token is valid
+      const payload = jwt.verify(token, process.env.JWT_SECRET);
+      if (!payload) {
+        return res.status(401).json("Unauthorized access");
+      }
+  
+      // Checking if user is valid
+      const user = await userModel.getUserById(payload.user.id);
+      if (!user) {
+        throw new Error("User no longer exists");
+      }
+  
+      // Providing user for the next in the pipeline
+      req.user = user;
+      next();
     } catch (err) {
-        return res.status(401).json("unauthorized access")
-    };
-};
+      return res.status(401).json("Unauthorized access");
+    }
+  }
