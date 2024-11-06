96a82b49278580bb07706c5e59fa82cabbda6b06
Zygis911 <zygis.cer@gmail.com>
2024-11-06 23:03:01 +0200

    judam

--- Veterinary/src/components/Navbar.jsx
+++ Veterinary/src/components/Navbar.jsx
@@ -2,14 +2,24 @@ import './Navbar.css'
 import React , {useState} from "react";
 import {HiOutlineMenuAlt4} from 'react-icons/hi'
 import {FaRegTimesCircle} from 'react-icons/fa'
-import {BsFillHouseFill} from 'react-icons/bs'
+import { SiAnimalplanet } from "react-icons/si";
 
 
 export function Navbar() {
     return (
         <div className='navbar'>
             <div className='container'>
-                <h1><span><BsFillHouseFill />Real</span>Estate</h1>
+                <h1><span><SiAnimalplanet />Animals </span></h1>
+                <button className='btn'>Sign in</button>
+                <ul className='nav-menu'>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Search</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Contacts</a></li>
+                </ul>
+                <div className='hamburger'>
+                    <HiOutlineMenuAlt4 />
+                </div>
             </div>
         </div>
     )
