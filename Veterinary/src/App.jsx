12a8cb03d3e208943dd66ff4c90f9c3a4a9a06d3
Zygis911 <zygis.cer@gmail.com>
2024-11-20 16:06:46 +0200

    judesiukas

--- Veterinary/src/App.jsx
+++ Veterinary/src/App.jsx
@@ -5,14 +5,18 @@ import { Hero } from "./components/HERO/Hero";
 import { Best } from "./components/best/Best";
 import { Featured } from "./components/FEATURED/Featured";
 import { Footer } from "./components/FOOTER/Footer";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
+      {/* Define your routing structure */}
       <Navbar />
-      <Hero />
-      <Best />
-      <Featured />
+      <Routes>
+        <Route path="/" element={<Hero />} />
+        <Route path="/best" element={<Best />} />
+        <Route path="/featured" element={<Featured />} />
+      </Routes>
       <Footer />
     </>
   );
--- Veterinary/src/components/NAVBAR/Navbar.jsx
+++ Veterinary/src/components/NAVBAR/Navbar.jsx
@@ -3,6 +3,7 @@ import React , {useState} from "react";
 import {HiOutlineMenuAlt4} from 'react-icons/hi'
 import {FaRegTimesCircle} from 'react-icons/fa'
 import { SiAnimalplanet } from "react-icons/si";
+import { Link } from "react-router-dom";
 
 
 export function Navbar() {
@@ -16,13 +17,13 @@ export function Navbar() {
         <div className='navbar'>
             <div className='container'>
                 <h1><span><SiAnimalplanet />Animals </span></h1>
-                <button className='btn'>Sign in</button>
                 <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#">Search</a></li>
-                    <li><a href="#">About</a></li>
+                    <li><Link to='/'>Home</Link></li>
+                    <li><Link to='/best'>Treatments</Link></li>
+                    <li><Link to='/featured'>Animals</Link></li>
                     <li><a href="#">Contacts</a></li>
-                </ul>
+                    <button className='btn'>Sign in</button>
+                </ul>             
                 <div className='hamburger' onClick={handleClick}>
                     {click ? (<FaRegTimesCircle className='icon'/>) : (<HiOutlineMenuAlt4 className='icon' />)}
                 </div>
